1bc3dc5e488bb32ac49cf80f38795bf973a8a5b0
Joseph C Wang <joequant@gmail.com>
2026-02-19 11:46:32 +0800

    add docker fixes

--- papers/governance/startup.sh
+++ papers/governance/startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ ! -d "axc-backtesting" ]; then
+    git clone http://github.com/accelerate-protocol/axc-backtesting.git
+fi
 cd axc-backtesting
-git pull
+git fetch origin ${GIT_BRANCH:-main}
+git checkout ${GIT_BRANCH:-main}
+git reset --hard origin/${GIT_BRANCH:-main}
 cd papers/governance
 ./splitfile.py source.md --delimiter "# %" --overwrite
 myst build --html
